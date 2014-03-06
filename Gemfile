f0cf89671770c7cd2e6e019d81e4de437fa14192
Dan Rasband <danrasband@gmail.com>
2014-03-06 13:11:43 -0700

    Add slim, layout, and index template

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'puma'
+gem 'slim'
 
--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require 'slim'
 
 configure { set :server, :puma }
 
 class Pumatra < Sinatra::Base
   get '/' do
-    request.ip
+    @ip = request.ip
+    @title = @ip
+
+    slim :index
   end
 
   run! if app_file == $0
